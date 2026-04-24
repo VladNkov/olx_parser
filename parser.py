c96d28472e3ca3e337f9e195d76a264cf0f528da
VladNkov <vladnkov88@gmail.com>
2026-04-24 23:41:53 +0200

    added id and selectors for price in function parse_ad

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
 import asyncio
+import re
 
 START_URL = 'https://www.olx.ua/uk/nedvizhimost/kvartiry/'
 PAGES = 1
@@ -13,12 +14,19 @@ async def parse_ad(browser, link):
     await ad_page.goto(link, timeout=60000)
     await ad_page.wait_for_timeout(3000)
 
-    title = ''
+
     price = ''
     data_parametrs ={}
 
-    if await ad_page.locator('[data-testid="ad-price-container"]').count() > 0:
-        price = await ad_page.locator('[data-testid="ad-price-container"]').inner_text()
+    match = re.search(r'-(ID[^./?]+)\.html', link)
+    ad_id = match.group(1) if match else ''
+
+    price_selectors = ['[data-testid="ad-price-container"]', '[data-testid="ad-price"]', 'h3',]
+
+    for selector in price_selectors:
+        if await ad_page.locator('[data-testid="ad-price-container"]').count() > 0:
+            price = await ad_page.locator('[data-testid="ad-price-container"]').inner_text()
+            break
 
     params = ad_page.locator('p[data-nx-name="P3"]')
     count = await params.count()
@@ -30,9 +38,10 @@ async def parse_ad(browser, link):
             key, value = text.split(':', 1)
             data_parametrs[key.strip()] = value.strip()
 
-
-
-    ad_data = {'title': title, 'price': price, 'object_type': data_parametrs.get("Вид об'єкта", ''), 'url': link,}
+    ad_data = {'ad_id': ad_id,
+               'price': price,
+               'object_type': data_parametrs.get("Вид об'єкта", ''),
+               'url': link,}
 
     await ad_page.close()
 
